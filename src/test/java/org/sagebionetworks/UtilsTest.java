5fae5997c4e488a4a6d033a97dd4862c9280e61b
bhoff <bruce.hoff@sagebase.org>
2019-03-06 13:07:55 -0800

    https://github.com/Sage-Bionetworks/SynapseWorkflowHook/issues/42

--- src/test/java/org/sagebionetworks/UtilsTest.java
+++ src/test/java/org/sagebionetworks/UtilsTest.java
@@ -1,7 +1,14 @@
 package org.sagebionetworks;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.sagebionetworks.Constants.SUBMISSION_FAILED;
+import static org.sagebionetworks.Constants.SUBMISSION_NOTIFICATION_MASK_PARAM_NAME;
+import static org.sagebionetworks.Constants.SUBMISSION_STARTED;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.sagebionetworks.repo.model.EntityBundle;
 import org.sagebionetworks.repo.model.docker.DockerRepository;
@@ -10,7 +17,16 @@ import org.sagebionetworks.schema.adapter.org.json.JSONObjectAdapterImpl;
 
 
 public class UtilsTest {
-
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		System.clearProperty(SUBMISSION_NOTIFICATION_MASK_PARAM_NAME);
+	}
+	
 	@Test
 	public void testGetDockerRepositoryNameFromEntityBundle() throws Exception {
 		String repoName = "docker.synapse.org/syn1234/foo";
@@ -26,6 +42,14 @@ public class UtilsTest {
 	@Test
 	public void testGetProgressFromString() {
 		assertEquals(92.85D, Utils.getProgressPercentFromString("STDOUT: 92.85714285714286", 5), 1e-5);
+	}
+	
+	@Test
+	public void testNotificationEnabled() {
+		System.setProperty(SUBMISSION_NOTIFICATION_MASK_PARAM_NAME, "28");
+		assertFalse(Utils.notificationEnabled(SUBMISSION_STARTED));
+		assertFalse(Utils.notificationEnabled(SUBMISSION_STARTED));
+		assertTrue(Utils.notificationEnabled(SUBMISSION_FAILED));
 	}
 
 }
